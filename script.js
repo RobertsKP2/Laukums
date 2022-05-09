c02adace8168c7eda73f167072b2c810ae810cb0
RobertsKP2 <98320392+RobertsKP2@users.noreply.github.com>
2022-05-09 15:35:40 +0300

    8

--- script.js
+++ script.js
@@ -34,22 +34,29 @@ function laukums(m1, m2, m3) {
     console.log(tlaukums);
     return tlaukums;
 }
-    function rezultats() {
-        const m = nolasa();
-        m1 = m.m1; m2 = m.m2; m3 = m.m3;
-        console.log(m1, m2, m3);
-        if (!nolasa()) {
-            t = "Nekorekti ievaddati, trijstūra malu vērtībām ir jābūt lielākām par 0";
+function irTrijsturis(m1, m2, m3) {
+    if (m1 < m2 + m3 && m2 < m1 + m3 && m3 < m1 + m2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function rezultats() {
+    const m = nolasa();
+    m1 = m.m1; m2 = m.m2; m3 = m.m3;
+    console.log(m1, m2, m3);
+    if (!nolasa()) {
+        t = "Nekorekti ievaddati, trijstūra malu vērtībām ir jābūt lielākām par 0";
+    } else {
+        if ((irTrijsturis)(m1, m2, m3) == false) {
+            t = "Trijsturis neeksistē, jo jebkuru 2 malu garumu summai ir jābūt lielākai par trešo malu!";
         } else {
-            if ((irTrijsturis)(m1, m2, m3) == false) {
-                t = "Trijsturis neeksistē, jo jebkuru 2 malu garumu summai ir jābūt lielākai par trešo malu!";
-            } else {
-                t = "Trijstūris ar malu garumiem " + m1 + ", " + m2 + "un " + m3 + " eksistē."
-                if (m1 == m2 && m2 == m3) {
-                    t += "Tas ir vienādsānu trijstūris."
+            t = "Trijstūris ar malu garumiem " + m1 + ", " + m2 + "un " + m3 + " eksistē."
+            if (m1 == m2 && m2 == m3) {
+                t += "Tas ir vienādsānu trijstūris."
 
-                }
-            
+            }
+
             const p = perimetrs(m1, m2, m3);
             const s = Math.round(laukums(m1, m2, m3) * 100) / 100;
             t += "Perimetrs ir " + p + " un laukums ir " + s + ".";
@@ -57,7 +64,7 @@ function laukums(m1, m2, m3) {
     }
     console.log(t);
     return t;
-    }
+}
 function izvadaTekstu() {
     const teksts = rezultats();
     console.log(teksts);
